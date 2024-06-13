b4689e01a465b9257fe15adb7875ec93be115afe
Danila Kurach <danyakurach@gmail.com>
2024-06-13 19:52:13 +0300

    feat: added schema

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -14,7 +14,94 @@ type storage struct {
 }
 
 var schema = `
-	
+	CREATE TABLE IF NOT EXISTS users (
+	    							 id serial primary key,
+	    							 email text not null,
+	                                 password_hash text not null,
+	                                 password_salt text not null,
+	                                 is_recruiter boolean not null default false,
+	                                 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS vacancies (
+	    							 id serial primary key,
+	    							 recruiter_id integer not null references users(id),
+	    							 experience text,
+	    							 city text,
+	    							 employment_type text,
+	    							 salary_from integer,
+	    							 salary_to integer,
+	    							 company_name text,
+	    							 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS applications (
+	    							 id serial primary key,
+	    							 vacancy_id bigint not null references vacancies(id),
+	    							 user_id bigint not null references users(id),
+	    							 stage text,
+	    							 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS resume (
+	    							 id serial primary key,
+	    							 user_id bigint not null references users(id),
+	    							 job_name text not null,
+									 gender text not null,
+									 address text, 
+									 birth_date timestamptz,
+									 phone_number text,
+									 salary_from integer,
+	    							 salary_to integer,
+									 education text, 
+									 skills []text, 
+									 nationality text,
+									 disability boolean,
+									 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS experience (
+	    							 id serial primary key,
+	    							 resume_id bigint not null references resume(id),
+	    							 company_name text,
+	    							 time_from timestamptz,
+	    							 time_to timestamptz,
+									 position text,
+									 work_exp_description text,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS notifications (
+	    							 id serial primary key,
+	    							 user_id bigint not null references users(id),
+	    							 type text,
+	    							 message text,
+	    							 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS meeting (
+	    							 id serial primary key,
+	    							 application_id bigint not null references applications(id),
+	    							 time timestamptz,
+	    							 name text,
+	    							 description text,
+	    							 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
+
+	CREATE TABLE IF NOT EXISTS time_slots (
+	    							 id serial primary key,
+	    							 recruiter_id bigint not null references users(id),
+	    							 time_from timestamptz,
+	    							 time_to timestamptz,
+	    							 occupied boolean,
+	    							 archived boolean,
+	    							 created_at timestamptz not null default now()
+	);
 `
 
 // New creates a new instance of database layer using Postgres
