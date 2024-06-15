9f362c9c698387d4167b51ef1242e20865ae47f8
Danila Kurach <danyakurach@gmail.com>
2024-06-15 22:17:51 +0300

    feat: logger update

--- internal/middleware/request_logger.go
+++ internal/middleware/request_logger.go
@@ -8,7 +8,7 @@ import (
 
 func (m middleware) RequestLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		slog.Debug("Request",
+		slog.Info("Request",
 			slog.Any("method", c.Request().Method),
 			slog.Any("host", c.Request().Host),
 			slog.Any("remote addr", c.Request().RemoteAddr),
